658f1e4578b87a542ed3bd990ec5027a430bac67
kgill57 <mcgillkd@dukes.jmu.edu>
2018-02-11 14:21:09 -0500

    Login Update

---
+++ Lab4Website/AdminPage.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="AdminPage.aspx.cs" Inherits="AdminPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <h1>This is the admin page</h1>
+</asp:Content>
+
--- Lab4Website/LoginPage.aspx.cs
+++ Lab4Website/LoginPage.aspx.cs
@@ -7,6 +7,8 @@ using System.Web.UI.WebControls;
 
 public partial class LoginPage : System.Web.UI.Page
 {
+    string adminUser = "admin";
+    string adminPass = "admin";
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -15,5 +17,13 @@ public partial class LoginPage : System.Web.UI.Page
     protected void btnExit_Click(object sender, EventArgs e)
     {
         Environment.Exit(0);
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        if(txtUsername.Text.Equals(adminUser) && txtPassword.Text.Equals(adminPass))
+        {
+            Response.Redirect("AdminPage.aspx");
+        }
     }
 }

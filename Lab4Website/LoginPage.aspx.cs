07795119bdcf78048173b4dcfebd6fc4a153ad24
bennskych <36310932+bennskych@users.noreply.github.com>
2018-02-13 16:24:20 -0500

    Add files via upload

--- Lab4Website/LoginPage.aspx.cs
+++ Lab4Website/LoginPage.aspx.cs
@@ -4,14 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 public partial class LoginPage : System.Web.UI.Page
 {
-    string adminUser = "admin";
-    string adminPass = "admin";
     protected void Page_Load(object sender, EventArgs e)
     {
+        //SqlConnection con = new SqlConnection();
+        //con.ConnectionString = "Server=localhost;Database=Lab4;Trusted_Connection=Yes;";
+        //con.Open();
 
+        //SqlCommand select = new SqlCommand();
+        //select.Connection = con;
+
+        //select.CommandText = "INSERT INTO [dbo].[User] VALUES('Chris', 'J', 'Bennsky', 'Bennskych@gmail.com', 'admin', NULL, 1, NULL, 'Bennsky', '2018-01-01')";
+        //select.ExecuteNonQuery();
+
+        //string password = "password";
+
+        //string passwordHashNew =
+        //           SimpleHash.ComputeHash(password, "MD5", null);
+
+        //select.CommandText = "INSERT INTO[dbo].[Password] Values (2, '" + passwordHashNew + "')";
+        //select.ExecuteNonQuery();
+        //con.Close();
     }
 
     protected void btnExit_Click(object sender, EventArgs e)
@@ -21,9 +38,36 @@ public partial class LoginPage : System.Web.UI.Page
 
     protected void btnLogin_Click(object sender, EventArgs e)
     {
-        if(txtUsername.Text.Equals(adminUser) && txtPassword.Text.Equals(adminPass))
+        String username = txtUsername.Text;
+        String password = txtPassword.Text;
+
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = "Server=localhost;Database=Lab4;Trusted_Connection=Yes;";
+        con.Open();
+
+        SqlCommand select = new SqlCommand();
+        select.Connection = con;
+
+        select.CommandText = "SELECT [PasswordHash] FROM [dbo].[Password] WHERE [UserID] = (SELECT [UserID] FROM [dbo].[User] WHERE [UserName] = '" + username + "')";
+        String hash = (String)select.ExecuteScalar();
+
+        bool admin;
+        select.CommandText = "(SELECT [Admin] FROM [dbo].[User] WHERE [UserName] = '" + username + "')";
+        admin = Convert.ToBoolean(select.ExecuteScalar());
+        con.Close();
+
+        bool verify = SimpleHash.VerifyHash(password, "MD5", hash);
+
+        if (verify)
         {
-            Response.Redirect("AdminPage.aspx");
+            if(admin)
+            {
+                Response.Redirect("AdminPage.aspx");
+            }
+            else
+            {
+                //Send to user page
+            }
         }
         else
         {

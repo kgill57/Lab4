a1bfdff86bb0aeedea67ac012039ed59edb5c160
kgill57 <mcgillkd@dukes.jmu.edu>
2018-02-24 12:10:19 -0500

    Style changes to admin homepage and user options

--- Lab4Website/AdminPage.aspx.cs
+++ Lab4Website/AdminPage.aspx.cs
@@ -58,6 +58,7 @@ public partial class AdminPage : System.Web.UI.Page
             panelPost[i] = new Panel();
 
             Label[] labelPost = new Label[5];
+            
 
             labelPost[0] = new Label();
 
@@ -77,24 +78,28 @@ public partial class AdminPage : System.Web.UI.Page
                 labelPost[0].Text = (giver + " gifted " + reciever + " $" + transaction[i].getRewardValue());
             }
             panelPost[i].Controls.Add(labelPost[0]);
+            labelPost[0].Style.Add("padding-left", "10px");
 
             panelPost[i].Controls.Add(new LiteralControl("<br />"));
 
             labelPost[1] = new Label();
             labelPost[1].Text = ("Value: " + transaction[i].getValue());
             panelPost[i].Controls.Add(labelPost[1]);
+            labelPost[1].Style.Add("padding-left", "10px");
 
             panelPost[i].Controls.Add(new LiteralControl("<br />"));
 
             labelPost[2] = new Label();
             labelPost[2].Text = ("Category: " + transaction[i].getCategory());
             panelPost[i].Controls.Add(labelPost[2]);
+            labelPost[2].Style.Add("padding-left", "10px");
 
             panelPost[i].Controls.Add(new LiteralControl("<br />"));
 
             labelPost[3] = new Label();
             labelPost[3].Text = ("Description: " + transaction[i].getDescription());
             panelPost[i].Controls.Add(labelPost[3]);
+            labelPost[3].Style.Add("padding-left", "10px");
 
             panelPost[i].Controls.Add(new LiteralControl("<br />"));
 
@@ -102,14 +107,15 @@ public partial class AdminPage : System.Web.UI.Page
             TimeSpan difference = DateTime.Now - transaction[i].getPostDate();
             labelPost[4].Text = "Posted " + Convert.ToString((int)difference.TotalMinutes) + " Minutes Ago";
             panelPost[i].Controls.Add(labelPost[4]);
+            labelPost[4].Style.Add("padding-left", "10px");
 
             panelPost[i].BorderStyle = BorderStyle.Solid;
+            panelPost[i].Style.Add("padding", "10px");
 
             panelPost[i].CssClass = "postCSS";
 
 
             Panel1.Controls.Add(panelPost[i]);
-
         }
 
         con.Close();

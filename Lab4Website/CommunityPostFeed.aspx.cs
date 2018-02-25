9277e5a34cc722f5b77b3598ba3cf59a6f8b35d2
sdbasketball96 <johnathonhoyns@gmail.com>
2018-02-25 13:58:40 -0500

    Add files via upload

--- Lab4Website/CommunityPostFeed.aspx.cs
+++ Lab4Website/CommunityPostFeed.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
+using System.Configuration;
 
 public partial class CommunityPostFeed : System.Web.UI.Page
 {
@@ -24,83 +25,59 @@ public partial class CommunityPostFeed : System.Web.UI.Page
         if (!IsPostBack)
         {
             SqlConnection con = new SqlConnection();
-            con.ConnectionString = "Data Source=localhost;Initial Catalog=lab4;Integrated Security=True";
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["lab4ConnectionString"].ConnectionString;
             con.Open();
 
-            SqlCommand read = new SqlCommand("SELECT * FROM [dbo].[TRANSACTION] WHERE GiverID=" + (int)Session["UserID"] + " OR " +
-                "ReceiverID=" + (int)Session["UserID"] + " ORDER BY [TransID] DESC", con);
+            SqlCommand read = new SqlCommand("SELECT * FROM [dbo].[EventPost] ORDER BY [EventPostID] DESC", con);
 
             //Create Scaler to see how many transactions there are
-            SqlCommand scaler = new SqlCommand("SELECT COUNT(TransID) FROM [dbo].[TRANSACTION] WHERE GiverID=" + (int)Session["UserID"] + " OR " +
-                "ReceiverID=" + (int)Session["UserID"], con);
+            SqlCommand scaler = new SqlCommand("SELECT COUNT(EventPostID) FROM [dbo].[EventPost]", con);
 
             int arraySize = (int)scaler.ExecuteScalar();
 
             SqlDataReader reader = read.ExecuteReader();
 
-            Post[] transaction = new Post[arraySize];
+            EventPost[] transaction = new EventPost[arraySize];
             int arrayCounter = 0;
             while (reader.Read())
             {
-                transaction[arrayCounter] = new Post(Convert.ToInt32(reader.GetValue(0)), Convert.ToString(reader.GetValue(1)),
-                    Convert.ToString(reader.GetValue(2)), Convert.ToString(reader.GetValue(3)), Convert.ToDouble(reader.GetValue(4)), Convert.ToDateTime(reader.GetValue(5)), Convert.ToBoolean(reader.GetValue(6)), Convert.ToInt32(reader.GetValue(7)), Convert.ToInt32(reader.GetValue(8)));
+                transaction[arrayCounter] = new EventPost(Convert.ToInt32(reader.GetValue(0)), Convert.ToString(reader.GetValue(1)),
+                    Convert.ToString(reader.GetValue(2)), Convert.ToDateTime(reader.GetValue(3)), Convert.ToString(reader.GetValue(4)));
                 arrayCounter++;
             }
             con.Close();
             Panel[] panelPost = new Panel[arraySize];
             con.Open();
-
+            DateTime dt;
+            String adminName;
+            
             for (int i = 0; i < arraySize; i++)
             {
                 panelPost[i] = new Panel();
 
-                Label[] labelPost = new Label[5];
+                Label[] labelPost = new Label[3];
 
                 labelPost[0] = new Label();
-
-                if (transaction[i].getIsPrivate() == true)
-                {
-                    labelPost[0].Text = ("Anonymous" + " gifted " + "Anonymous");
-                }
-                else
-                {
-
-                    SqlCommand select = new SqlCommand("SELECT [FName] + ' ' + [LName] FROM [dbo].[User] WHERE [UserID] = " + transaction[i].getGiverID(), con);
-                    String giver = (String)select.ExecuteScalar();
-
-                    select.CommandText = "SELECT [FName] + ' ' + [LName] FROM [dbo].[User] WHERE [UserID] = " + transaction[i].getReceiverID();
-                    String reciever = (String)select.ExecuteScalar();
-
-                    labelPost[0].Text = (giver + " gifted " + reciever + " $" + transaction[i].getRewardValue());
-                }
+                labelPost[0].Text = ("Event Title: " + transaction[i].getEventTitle());
                 panelPost[i].Controls.Add(labelPost[0]);
-
                 panelPost[i].Controls.Add(new LiteralControl("<br />"));
 
-                labelPost[1] = new Label();
-                labelPost[1].Text = ("Value: " + transaction[i].getValue());
-                panelPost[i].Controls.Add(labelPost[1]);
 
+                labelPost[1] = new Label();
+                labelPost[1].Text = ("Event Description: " + transaction[i].getEventDesc());
+                panelPost[i].Controls.Add(labelPost[1]);
                 panelPost[i].Controls.Add(new LiteralControl("<br />"));
 
                 labelPost[2] = new Label();
-                labelPost[2].Text = ("Category: " + transaction[i].getCategory());
+                dt = transaction[i].getDatePosted();
+                //Create Scaler to see how many transactions there are
+                SqlCommand UserName = new SqlCommand("SELECT [dbo].[User].[FName] + ' ' + [dbo].[User].[LName]  FROM [dbo].[EventPost], [dbo].[User] WHERE [dbo].[EventPost].AdminID=[dbo].[User].[UserID]"
+                    + " AND [dbo].[EventPost].EventPostID=" + transaction[i].getEventPostID(), con);
+
+                adminName = (String)UserName.ExecuteScalar();
+                labelPost[2].Text = ("Posted By " + adminName + " on " + String.Format("{0:dddd, MMMM d, yyyy}", dt));
                 panelPost[i].Controls.Add(labelPost[2]);
-
                 panelPost[i].Controls.Add(new LiteralControl("<br />"));
-
-                labelPost[3] = new Label();
-                labelPost[3].Text = ("Description: " + transaction[i].getDescription());
-                panelPost[i].Controls.Add(labelPost[3]);
-
-                panelPost[i].Controls.Add(new LiteralControl("<br />"));
-
-                labelPost[4] = new Label();
-
-                TimeSpan difference = DateTime.Now - transaction[i].getPostDate();
-                labelPost[4].Text = "Posted " + Convert.ToString((int)difference.TotalMinutes) + " Minutes Ago";
-
-                panelPost[i].Controls.Add(labelPost[4]);
 
                 panelPost[i].BorderStyle = BorderStyle.Solid;
 
@@ -114,280 +91,5 @@ public partial class CommunityPostFeed : System.Web.UI.Page
             con.Close();
         }
 
-    }
-
-
-    protected void giverAndReceiver_SelectedIndexChanged(object sender, EventArgs e)
-    {
-        if(giverAndReceiver.SelectedIndex == 0)
-        {
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = "Data Source=localhost;Initial Catalog=lab4;Integrated Security=True";
-            con.Open();
-
-            SqlCommand read = new SqlCommand("SELECT * FROM [dbo].[TRANSACTION] WHERE GiverID=" + (int)Session["UserID"] + " OR " +
-                "ReceiverID=" + (int)Session["UserID"] + " ORDER BY [TransID] DESC", con);
-
-            //Create Scaler to see how many transactions there are
-            SqlCommand scaler = new SqlCommand("SELECT COUNT(TransID) FROM [dbo].[TRANSACTION] WHERE GiverID=" + (int)Session["UserID"] + " OR " +
-                "ReceiverID=" + (int)Session["UserID"], con);
-
-            int arraySize = (int)scaler.ExecuteScalar();
-
-            SqlDataReader reader = read.ExecuteReader();
-
-            Post[] transaction = new Post[arraySize];
-            int arrayCounter = 0;
-            while (reader.Read())
-            {
-                transaction[arrayCounter] = new Post(Convert.ToInt32(reader.GetValue(0)), Convert.ToString(reader.GetValue(1)),
-                    Convert.ToString(reader.GetValue(2)), Convert.ToString(reader.GetValue(3)), Convert.ToDouble(reader.GetValue(4)), Convert.ToDateTime(reader.GetValue(5)), Convert.ToBoolean(reader.GetValue(6)), Convert.ToInt32(reader.GetValue(7)), Convert.ToInt32(reader.GetValue(8)));
-                arrayCounter++;
-            }
-            con.Close();
-            Panel[] panelPost = new Panel[arraySize];
-            con.Open();
-
-            for (int i = 0; i < arraySize; i++)
-            {
-                panelPost[i] = new Panel();
-
-                Label[] labelPost = new Label[5];
-
-                labelPost[0] = new Label();
-
-                if (transaction[i].getIsPrivate() == true)
-                {
-                    labelPost[0].Text = ("Anonymous" + " gifted " + "Anonymous");
-                }
-                else
-                {
-
-                    SqlCommand select = new SqlCommand("SELECT [FName] + ' ' + [LName] FROM [dbo].[User] WHERE [UserID] = " + transaction[i].getGiverID(), con);
-                    String giver = (String)select.ExecuteScalar();
-
-                    select.CommandText = "SELECT [FName] + ' ' + [LName] FROM [dbo].[User] WHERE [UserID] = " + transaction[i].getReceiverID();
-                    String reciever = (String)select.ExecuteScalar();
-
-                    labelPost[0].Text = (giver + " gifted " + reciever + " $" + transaction[i].getRewardValue());
-                }
-                panelPost[i].Controls.Add(labelPost[0]);
-
-                panelPost[i].Controls.Add(new LiteralControl("<br />"));
-
-                labelPost[1] = new Label();
-                labelPost[1].Text = ("Value: " + transaction[i].getValue());
-                panelPost[i].Controls.Add(labelPost[1]);
-
-                panelPost[i].Controls.Add(new LiteralControl("<br />"));
-
-                labelPost[2] = new Label();
-                labelPost[2].Text = ("Category: " + transaction[i].getCategory());
-                panelPost[i].Controls.Add(labelPost[2]);
-
-                panelPost[i].Controls.Add(new LiteralControl("<br />"));
-
-                labelPost[3] = new Label();
-                labelPost[3].Text = ("Description: " + transaction[i].getDescription());
-                panelPost[i].Controls.Add(labelPost[3]);
-
-                panelPost[i].Controls.Add(new LiteralControl("<br />"));
-
-                labelPost[4] = new Label();
-
-                TimeSpan difference = DateTime.Now - transaction[i].getPostDate();
-                labelPost[4].Text = "Posted " + Convert.ToString((int)difference.TotalMinutes) + " Minutes Ago";
-
-                panelPost[i].Controls.Add(labelPost[4]);
-
-                panelPost[i].BorderStyle = BorderStyle.Solid;
-
-                panelPost[i].CssClass = "postCSS";
-
-
-                Panel1.Controls.Add(panelPost[i]);
-
-            }
-
-            con.Close();
-        }
-        else if(giverAndReceiver.SelectedIndex == 1)
-        {
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = "Data Source=localhost;Initial Catalog=lab4;Integrated Security=True";
-            con.Open();
-
-            SqlCommand read = new SqlCommand("SELECT * FROM [dbo].[TRANSACTION] WHERE GiverID=" + (int)Session["UserID"] + " ORDER BY [TransID] DESC", con);
-
-            //Create Scaler to see how many transactions there are
-            SqlCommand scaler = new SqlCommand("SELECT COUNT(TransID) FROM [dbo].[TRANSACTION] WHERE GiverID=" + (int)Session["UserID"], con);
-            int arraySize = (int)scaler.ExecuteScalar();
-
-            SqlDataReader reader = read.ExecuteReader();
-
-            Post[] transaction = new Post[arraySize];
-            int arrayCounter = 0;
-            while (reader.Read())
-            {
-                transaction[arrayCounter] = new Post(Convert.ToInt32(reader.GetValue(0)), Convert.ToString(reader.GetValue(1)),
-                    Convert.ToString(reader.GetValue(2)), Convert.ToString(reader.GetValue(3)), Convert.ToDouble(reader.GetValue(4)), Convert.ToDateTime(reader.GetValue(5)), Convert.ToBoolean(reader.GetValue(6)), Convert.ToInt32(reader.GetValue(7)), Convert.ToInt32(reader.GetValue(8)));
-                arrayCounter++;
-            }
-            con.Close();
-            Panel[] panelPost = new Panel[arraySize];
-            con.Open();
-
-            for (int i = 0; i < arraySize; i++)
-            {
-                panelPost[i] = new Panel();
-
-                Label[] labelPost = new Label[5];
-
-                labelPost[0] = new Label();
-
-                if (transaction[i].getIsPrivate() == true)
-                {
-                    labelPost[0].Text = ("Anonymous" + " gifted " + "Anonymous");
-                }
-                else
-                {
-
-                    SqlCommand select = new SqlCommand("SELECT [FName] + ' ' + [LName] FROM [dbo].[User] WHERE [UserID] = " + transaction[i].getGiverID(), con);
-                    String giver = (String)select.ExecuteScalar();
-
-                    select.CommandText = "SELECT [FName] + ' ' + [LName] FROM [dbo].[User] WHERE [UserID] = " + transaction[i].getReceiverID();
-                    String reciever = (String)select.ExecuteScalar();
-
-                    labelPost[0].Text = (giver + " gifted " + reciever + " $" + transaction[i].getRewardValue());
-                }
-                panelPost[i].Controls.Add(labelPost[0]);
-
-                panelPost[i].Controls.Add(new LiteralControl("<br />"));
-
-                labelPost[1] = new Label();
-                labelPost[1].Text = ("Value: " + transaction[i].getValue());
-                panelPost[i].Controls.Add(labelPost[1]);
-
-                panelPost[i].Controls.Add(new LiteralControl("<br />"));
-
-                labelPost[2] = new Label();
-                labelPost[2].Text = ("Category: " + transaction[i].getCategory());
-                panelPost[i].Controls.Add(labelPost[2]);
-
-                panelPost[i].Controls.Add(new LiteralControl("<br />"));
-
-                labelPost[3] = new Label();
-                labelPost[3].Text = ("Description: " + transaction[i].getDescription());
-                panelPost[i].Controls.Add(labelPost[3]);
-
-                panelPost[i].Controls.Add(new LiteralControl("<br />"));
-
-                labelPost[4] = new Label();
-
-                TimeSpan difference = DateTime.Now - transaction[i].getPostDate();
-                labelPost[4].Text = "Posted " + Convert.ToString((int)difference.TotalMinutes) + " Minutes Ago";
-
-                panelPost[i].Controls.Add(labelPost[4]);
-
-                panelPost[i].BorderStyle = BorderStyle.Solid;
-
-                panelPost[i].CssClass = "postCSS";
-
-
-                Panel1.Controls.Add(panelPost[i]);
-
-            }
-
-            con.Close();
-        }
-        else if(giverAndReceiver.SelectedIndex == 2)
-        {
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = "Data Source=localhost;Initial Catalog=lab4;Integrated Security=True";
-            con.Open();
-
-            SqlCommand read = new SqlCommand("SELECT * FROM [dbo].[TRANSACTION] WHERE ReceiverID=" + (int)Session["UserID"] + " ORDER BY [TransID] DESC", con);
-
-            //Create Scaler to see how many transactions there are
-            SqlCommand scaler = new SqlCommand("SELECT COUNT(TransID) FROM [dbo].[TRANSACTION] WHERE ReceiverID=" + (int)Session["UserID"], con);
-            int arraySize = (int)scaler.ExecuteScalar();
-
-            SqlDataReader reader = read.ExecuteReader();
-
-            Post[] transaction = new Post[arraySize];
-            int arrayCounter = 0;
-            while (reader.Read())
-            {
-                transaction[arrayCounter] = new Post(Convert.ToInt32(reader.GetValue(0)), Convert.ToString(reader.GetValue(1)),
-                    Convert.ToString(reader.GetValue(2)), Convert.ToString(reader.GetValue(3)), Convert.ToDouble(reader.GetValue(4)), Convert.ToDateTime(reader.GetValue(5)), Convert.ToBoolean(reader.GetValue(6)), Convert.ToInt32(reader.GetValue(7)), Convert.ToInt32(reader.GetValue(8)));
-                arrayCounter++;
-            }
-            con.Close();
-            Panel[] panelPost = new Panel[arraySize];
-            con.Open();
-
-            for (int i = 0; i < arraySize; i++)
-            {
-                panelPost[i] = new Panel();
-
-                Label[] labelPost = new Label[5];
-
-                labelPost[0] = new Label();
-
-                if (transaction[i].getIsPrivate() == true)
-                {
-                    labelPost[0].Text = ("Anonymous" + " gifted " + "Anonymous");
-                }
-                else
-                {
-
-                    SqlCommand select = new SqlCommand("SELECT [FName] + ' ' + [LName] FROM [dbo].[User] WHERE [UserID] = " + transaction[i].getGiverID(), con);
-                    String giver = (String)select.ExecuteScalar();
-
-                    select.CommandText = "SELECT [FName] + ' ' + [LName] FROM [dbo].[User] WHERE [UserID] = " + transaction[i].getReceiverID();
-                    String reciever = (String)select.ExecuteScalar();
-
-                    labelPost[0].Text = (giver + " gifted " + reciever + " $" + transaction[i].getRewardValue());
-                }
-                panelPost[i].Controls.Add(labelPost[0]);
-
-                panelPost[i].Controls.Add(new LiteralControl("<br />"));
-
-                labelPost[1] = new Label();
-                labelPost[1].Text = ("Value: " + transaction[i].getValue());
-                panelPost[i].Controls.Add(labelPost[1]);
-
-                panelPost[i].Controls.Add(new LiteralControl("<br />"));
-
-                labelPost[2] = new Label();
-                labelPost[2].Text = ("Category: " + transaction[i].getCategory());
-                panelPost[i].Controls.Add(labelPost[2]);
-
-                panelPost[i].Controls.Add(new LiteralControl("<br />"));
-
-                labelPost[3] = new Label();
-                labelPost[3].Text = ("Description: " + transaction[i].getDescription());
-                panelPost[i].Controls.Add(labelPost[3]);
-
-                panelPost[i].Controls.Add(new LiteralControl("<br />"));
-
-                labelPost[4] = new Label();
-
-                TimeSpan difference = DateTime.Now - transaction[i].getPostDate();
-                labelPost[4].Text = "Posted " + Convert.ToString((int)difference.TotalMinutes) + " Minutes Ago";
-
-                panelPost[i].Controls.Add(labelPost[4]);
-
-                panelPost[i].BorderStyle = BorderStyle.Solid;
-
-                panelPost[i].CssClass = "postCSS";
-
-
-                Panel1.Controls.Add(panelPost[i]);
-
-            }
-
-            con.Close();
-        }
     }
 }
